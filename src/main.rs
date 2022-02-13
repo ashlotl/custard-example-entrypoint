57432a42499ac36a367418b8aadb93a692429779
Ashlotl <thesupremeliverwurst@gmail.com>
2022-02-13 00:23:17 -0800

    work on datachunk/crate loading

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ use custard_use::{
 fn main() -> Result<(), Box<dyn Error>> {
 	//tell custard_use to load all compositions and implicitly smush them together in from_string
 	let root_composition_string = get_maybe_const_string(concat!(env!("CARGO_MANIFEST_DIR"), "/src/composition.ron"), include_str!("composition.ron")).0;
-	let root_composition_unloaded = UnloadedComposition::from_string(root_composition_string)?;
+	let root_composition_unloaded = UnloadedComposition::from_string(root_composition_string, true, true)?;
 
 	println!("Full UnloadedComposition: {:#?}", root_composition_unloaded);
 
 	//tell lib to turn an unloaded composition (see above) into a loaded composition
-	let root_composition = LoadedComposition::check(root_composition_unloaded)?;
+	let root_composition = LoadedComposition::check(root_composition_unloaded, true, true)?;
 
 	println!("Full LoadedComposition: {:#?}", root_composition);
 
