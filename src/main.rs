53b52302a6ac7401e2e030a6cde24d90c9ad64cf
Ashlotl <thesupremeliverwurst@gmail.com>
2022-02-27 19:26:17 -0800

    Got datachunk loading working

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
-use std::error::Error;
+use std::{error::Error, sync::Arc};
 
 use custard_use::{
 	composition::{loaded::loaded_composition::LoadedComposition, unloaded::unloaded_composition::UnloadedComposition},
+	dylib_management::safe_library::safe_library::{DebugMode, LibraryRecompile},
 	utils::files::get_maybe_const_string,
 };
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Arc<dyn Error>> {
 	//tell custard_use to load all compositions and implicitly smush them together in from_string
 	let root_composition_string = get_maybe_const_string(concat!(env!("CARGO_MANIFEST_DIR"), "/src/composition.ron"), include_str!("composition.ron")).0;
-	let root_composition_unloaded = UnloadedComposition::from_string(root_composition_string, true, true)?;
+	let root_composition_unloaded = UnloadedComposition::from_string(root_composition_string, LibraryRecompile::Recompile, DebugMode::Debug)?;
 
 	println!("Full UnloadedComposition: {:#?}", root_composition_unloaded);
 
 	//tell lib to turn an unloaded composition (see above) into a loaded composition
-	let root_composition = LoadedComposition::check(root_composition_unloaded, true, true)?;
+	let root_composition = LoadedComposition::check(root_composition_unloaded, LibraryRecompile::Recompile, DebugMode::Debug)?;
 
 	println!("Full LoadedComposition: {:#?}", root_composition);
 
